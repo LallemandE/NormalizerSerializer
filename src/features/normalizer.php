78dceaa6955d80f70f5d5e925f5ead743a5be754
athena2018 <lesgayard@gmail.com>
2018-03-07 12:25:05 +0100

    add normalizer

---
+++ src/features/normalizer.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+// Object Normalizer
+class Normalizer
+{
+//public function Normalize => array with resultName as keys and result of methods on the object as value
+    public function normalize($InputObject, $metadata)
+    {
+        
+ // get the configuration of the object       
+        $config = $metadata->getConfiguration();
+// the intermediary object, neeeded to store the function results values        
+        $intermediaryObject = [];
+        
+// foreach line into the array
+        foreach ($config as $methodName =>$values){
+            $other = $InputObject->$methodName(); 
+            $intermediaryObject[$values] = $other;
+        }
+        
+        
+        return $intermediaryObject;
+    }
+}
+
+
+ 
+ 
+ 
+
+     
+     
+ 
+ 
+ 
+ 
+
+
+?>
